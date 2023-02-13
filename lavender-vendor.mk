58a3e7ef511243b8a96a58b734ae9fa7a1a06fbd
ariffjenong <arifbuditantodablekk@gmail.com>
2023-02-13 08:14:51 +0700

    lavender: Import libtinyxml from LA.UM.9.6.2.r1-04100-89xx.0 • As the repo is missing from source, we can't really build it.   So, better add it prebuilt from the same CAF tag as possessed by Post Processing blobs. • Eliminates the following log spam
    F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/mm-pp-dpps": library "libtinyxml.so" not found: needed by main executable
    
    Signed-off-by: ariffjenong <arifbuditantodablekk@gmail.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1387,6 +1387,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/libthermalfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libthermalfeature.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/libthermalioctl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libthermalioctl.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/libtime_genoff.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libtime_genoff.so \
+vendor/xiaomi/lavender/proprietary/vendor/lib64/libtinyxml.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libtinyxml.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/libtinyxml2_1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libtinyxml2_1.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so \
     vendor/xiaomi/lavender/proprietary/vendor/lib64/libwms.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwms.so \
